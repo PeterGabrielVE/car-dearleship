4c193dad7caeb7842277d8f3460c18c99258ec3e
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-12 12:24:25 -0400

    servicios

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+    
     @Get()
     getAllCars(): string[]{
-        return ['toyota','spark','Jeep'];
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string ){
+        console.log({ id })
+        return  this.cars[id];
     }
 }
