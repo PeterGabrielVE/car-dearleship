c4866d550d4a94de3a1b8988cbe511f24b3975b6
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-07-12 14:43:36 -0400

    Pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -15,8 +15,8 @@ export class CarsController {
     }
 
     @Get(':id')
-    getCarById( @Param('id') id: string ){
+    getCarById( @Param('id', ParseIntPipe) id: number ){
         console.log({ id })
-        return  this.carsService.findOneByID(+id);
+        return  this.carsService.findOneByID( id );
     }
 }
